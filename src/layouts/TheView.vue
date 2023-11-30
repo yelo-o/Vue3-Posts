23208081ce03f857b69db0782e0bc13ef78fafb6
yelo-o <flyordig@gmail.com>
2023-11-30 23:51:23 +0900

    첫번째 커밋

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,17 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<h2>Hello Vue3</h2>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
