1fbda1ce25c98b2393e35e4e3bbda5b535b1d739
yelo-o <flyordig@gmail.com>
2023-12-03 02:01:08 +0900

    push와 replace의 차이 : 히스토리 기록 여부

--- src/views/nested/NestedView.vue
+++ src/views/nested/NestedView.vue
@@ -3,13 +3,21 @@
 		<ul class="nav nav-pills">
 			<li class="nav-item">
 				<!-- <a class="nav-link active" aria-current="page" href="#">Nested One</a> -->
-				<RouterLink class="nav-link" active-class="active" to="/nested/one">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedOne', replace: true }"
+				>
 					Nested One
 				</RouterLink>
 			</li>
 			<li class="nav-item">
 				<!-- <a class="nav-link" href="#">Nested Two</a> -->
-				<RouterLink class="nav-link" active-class="active" to="/nested/two">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedTwo', replace: true }"
+				>
 					Nested Two
 				</RouterLink>
 			</li>
