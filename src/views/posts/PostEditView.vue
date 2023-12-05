da4f2182b6001ccc14e8a8d197793d170d8138df
yelo-o <flyordig@gmail.com>
2023-12-05 18:42:34 +0900

    다중 메시지에 대한 TransitionGroup 컴포넌트 적용

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,7 +18,7 @@
 				<button class="btn btn-primary">수정</button>
 			</template>
 		</PostForm>
-		<AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
+		<AppAlert :items="alerts" />
 	</div>
 </template>
 
@@ -43,7 +43,7 @@ const fetchPost = async () => {
 		setForm(data);
 	} catch (error) {
 		console.error(error);
-		vAlert('네트워크 오류!');
+		vAlert(error.message);
 	}
 };
 const setForm = ({ title, content }) => {
@@ -54,10 +54,10 @@ fetchPost();
 const edit = async () => {
 	try {
 		await updatePost(id, { ...form.value });
-		// router.push({ name: 'PostDetail', params: { id } });
-		vAlert('수정이 완료되었습니다!!!', 'success');
+		vAlert('수정이 완료되었습니다!', 'success');
 	} catch (error) {
 		console.error(error);
+		vAlert(error.message);
 	}
 };
 
@@ -67,15 +67,11 @@ const goDetailPage = () =>
 		params: { id },
 	});
 //alert
-const showAlert = ref(false);
-const alertMessage = ref('');
-const alertType = ref();
+const alerts = ref([]);
 const vAlert = (message, type = 'error') => {
-	showAlert.value = true;
-	alertMessage.value = message;
-	alertType.value = type;
+	alerts.value.push({ message, type });
 	setTimeout(() => {
-		showAlert.value = false;
+		alerts.value.shift();
 	}, 2000);
 };
 </script>
