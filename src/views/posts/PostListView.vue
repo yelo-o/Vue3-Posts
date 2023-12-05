4d58f4e6e6411b9868f7568708a0a400f1b4f83f
yelo-o <flyordig@gmail.com>
2023-12-05 14:44:10 +0900

    Filter 컴포넌트 분리

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -2,31 +2,22 @@
 	<div>
 		<h2>게시글 목록</h2>
 		<hr class="my-4" />
-		<form @submit.prevent>
-			<div class="row g-3">
-				<div class="col">
-					<input v-model="params.title_like" type="text" class="form-control" />
-				</div>
-				<div class="col-3">
-					<select v-model="params._limit" class="form-select">
-						<option value="3">3개씩 보기</option>
-						<option value="6">6개씩 보기</option>
-						<option value="9">9개씩 보기</option>
-					</select>
-				</div>
-			</div>
-		</form>
+
+		<PostFilter
+			v-model:title="params.title_like"
+			v-model:limit="params._limit"
+		/>
 		<hr class="my-4" />
-		<div class="row g-3">
-			<div v-for="post in posts" :key="post.id" class="col-4">
+		<AppGrid :items="posts">
+			<template v-slot="{ item }">
 				<PostItem
-					:title="post.title"
-					:content="post.content"
-					:created-at="post.createdAt"
-					@click="goPage(post.id)"
+					:title="item.title"
+					:content="item.content"
+					:created-at="item.createdAt"
+					@click="goPage(item.id)"
 				></PostItem>
-			</div>
-		</div>
+			</template>
+		</AppGrid>
 		<AppPagination
 			:current-page="params._page"
 			:page-count="pageCount"
@@ -49,8 +40,10 @@
 <script setup>
 import PostItem from '@/components/posts/PostItem.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
-import AppPagination from '@/components/posts/AppPagination.vue';
+import PostFilter from '@/components/posts/PostFilter.vue';
+import AppPagination from '@/components/AppPagination.vue';
 import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
 import { ref, computed, watchEffect } from 'vue';
 import { getPosts } from '@/api/posts';
 import { useRouter } from 'vue-router';
