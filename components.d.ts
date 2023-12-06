d4c6e1e6985b4427274ab7edd7496d97f3c0e637
yelo-o <flyordig@gmail.com>
2023-12-06 21:54:32 +0900

    Global 컴포넌트 인터페이스 정으

--- components.d.ts
+++ components.d.ts
@@ -1,6 +1,11 @@
 // components.d.ts
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AppAlert: typeof import('./src/components/app/AppAlert.vue')['default'];
+    AppCard: typeof import('./src/components/app/AppCard.vue')['default'];
+    AppGrid: typeof import('./src/components/app/AppGrid.vue')['default'];
+    AppModal: typeof import('./src/components/app/AppModal.vue')['default'];
+    AppPagination: typeof import('./src/components/app/AppPagination.vue')['default'];
     RouterLink: typeof import('vue-router')['RouterLink'];
     RouterView: typeof import('vue-router')['RouterView'];
   }
