994ccb43721037b566ae2fb1940876835b4cfa71
yelo-o <flyordig@gmail.com>
2023-12-06 10:12:22 +0900

    필요없는 코드 정리

--- src/components/posts/PostModal.vue
+++ src/components/posts/PostModal.vue
@@ -1,5 +1,5 @@
 <template>
-	<AppModal v-model="show" :show="show" title="게시글">
+	<AppModal v-model="show" title="게시글">
 		<template #default>
 			<div class="row g-3">
 				<div class="col-3 text-muted">제목</div>
